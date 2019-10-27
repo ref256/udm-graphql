221834324f07a244b5641440dd7c9a2ef723c668
greforce <greforce76@gmail.com>
2019-10-27 13:20:30 +0300

    initial basic setup of graphql server

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,66 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+// String, Boolean, Int, Float, ID
+
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    greeting(name: String, position: String): String!
+    add(a: Float!, b: Int!): Float!
+    me: User!
+    post: Post!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    greeting(parent, args, ctx, info) {
+      if (args.name && args.position) {
+        return `Hello, ${args.name}! You are my favourite ${args.position}`;
+      }
+      return 'Hello!';
+    },
+    add(_, args) {
+      return args.a + args.b;
+    },
+    me() {
+      return ({
+        id: 'user1',
+        name: 'Gregory',
+        email: 'greforce76@gmail.com'
+      });
+    },
+    post() {
+      return ({
+        id: 'post123',
+        title: 'My new post',
+        body: 'It is a test post, which I wrote',
+        published: false
+      });
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log('The server is up!');
+});
